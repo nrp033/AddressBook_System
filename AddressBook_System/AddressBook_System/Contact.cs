6b7a82582bf4efbe9eb747de6c46ffe7bc713b21
Nitin Pawar <107263213+nrp033@users.noreply.github.com>
2022-11-25 01:10:41 +0530

    Creating model class for contact details like First Name last Name address etc...

--- AddressBook_System/AddressBook_System/Contact.cs
+++ AddressBook_System/AddressBook_System/Contact.cs
@@ -9,13 +9,21 @@ namespace AddressBook_System
     internal class Contact
     {
         public string FirstName { get; set; }
+
         public string LastName { get; set; }
-        public string Address { get; set; }
-        public string City { get; set; }
-        public string State { get; set; }
-        public string Zip { get; set; }
+
         public string PhoneNumber { get; set; }
+
         public string Email { get; set; }
+
+        public string Address { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public string Zip { get; set; }
+       
 
     }
 }
